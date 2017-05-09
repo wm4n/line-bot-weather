e53380ff5ff72cda2184ce80dfcf689a7ea31dd6
William Chao <william@airsig.com>
2017-05-09 13:48:19 +0800

    Add debug log

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const linebot = require("linebot");
 const chat = require("./chat.js");
 
+// for test
+var bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 3123;
 const HEWEATHER_KEY = process.env.heweather_key;
 
@@ -24,6 +27,15 @@ bot.on('message', function(event) {
 // Create our app
 const app = express();
 const linebotParser = bot.parser();
+
+app.use(bodyParser.json())
+app.use(function (req, res) {
+  console.log(JSON.stringify(req.body, null, 2));
+  // res.setHeader('Content-Type', 'text/plain')
+  // res.write('you posted:\n')
+  // res.end(JSON.stringify(req.body, null, 2))
+})
+
 app.post('/', linebotParser);
 
 app.listen(PORT, function () {
