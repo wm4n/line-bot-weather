42fbe3f7a5419ae84f84ff31633e4828f1abf9a7
William Chao <william@airsig.com>
2017-07-14 16:43:58 +0800

    Make domain address env variable

--- src/ServerSettings.js
+++ src/ServerSettings.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    serverDomain: 'https://43697aef.ngrok.io',
+    serverDomain: process.env.server_domain,
     serverPort: process.env.PORT || 4321,
     googleKey: process.env.google_key,
     weatherKey: process.env.openWeatherMap_key 
